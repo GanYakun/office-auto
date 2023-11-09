95ac7f94e08c766635e04d1a19c800ac2b06e2cf
SunCanyang <try028@163.com>
2023-11-09 19:42:31 +0800

    审批

--- src/main/java/com/banfftech/events/SupplierApproveEvents.java
+++ src/main/java/com/banfftech/events/SupplierApproveEvents.java
@@ -37,7 +37,6 @@ public class SupplierApproveEvents {
         //当前的改为已处理
         Map<String, Object> updateWorkMap = UtilMisc.toMap("workEffortId", workEffortId, "currentStatusId", "PROCESSED");
         CommonUtils.setServiceFieldsAndRun(dispatcher.getDispatchContext(), updateWorkMap, "banfftech.updateWorkEffort", systemUser);
-
         //传递
         String transferTarget = getTransferTarget(delegator, target, boundEntity.getGenericValue());
         String nextWorkEffortId = delegator.getNextSeqId("WorkEffort");
@@ -46,30 +45,6 @@ public class SupplierApproveEvents {
         CommonUtils.setServiceFieldsAndRun(dispatcher.getDispatchContext(), createWorkMap, "banfftech.createWorkEffort", systemUser);
         dispatcher.runSync("banfftech.createWorkEffortPartyAssignment",
                 UtilMisc.toMap("userLogin", systemUser, "workEffortId", nextWorkEffortId, "partyId", transferTarget));
-    }
-
-
-    /**
-     *  申请人提交Vendor
-     */
-    public static void applicantSubmit(Map<String, Object> oDataContext, Map<String, Object> actionParameters, EdmBindingTarget edmBindingTarget)
-            throws GenericEntityException, OfbizODataException, GeneralServiceException, GenericServiceException {
-        Delegator delegator = (Delegator) oDataContext.get("delegator");
-        LocalDispatcher dispatcher = (LocalDispatcher) oDataContext.get("dispatcher");
-        GenericValue systemUser = Util.getSystemUser(delegator);
-        OdataOfbizEntity boundEntity = Util.getBoundEntity(actionParameters);
-        if (UtilValidate.isEmpty(boundEntity)) {
-            throw new OfbizODataException("Parameter error");
-        }
-        String partyId = (String) boundEntity.getPropertyValue("partyId");
-        String workEffortParentId = (String) boundEntity.getPropertyValue("workEffortParentId");
-        //分配给purchase
-        String nextWorkEffortId = delegator.getNextSeqId("WorkEffort");
-        Map<String, Object> createWorkMap = UtilMisc.toMap("partyId", partyId, "workEffortId", nextWorkEffortId,
-                "workEffortTypeId", "COWORK_TASK", "currentStatusId", "NOT_PROCESSED", "workEffortParentId", workEffortParentId);
-        CommonUtils.setServiceFieldsAndRun(dispatcher.getDispatchContext(), createWorkMap, "banfftech.createWorkEffort", systemUser);
-        dispatcher.runSync("banfftech.createWorkEffortPartyAssignment",
-                UtilMisc.toMap("userLogin", systemUser, "workEffortId", nextWorkEffortId, "partyId", "CG"));
     }
 
     private static String getTransferTarget(Delegator delegator, String target, GenericValue workEffort) throws GenericEntityException {
@@ -85,38 +60,10 @@ public class SupplierApproveEvents {
             GenericValue createUser = parentWorkEffort.getRelatedOne("UserLogin", false);
             return CommonUtils.getPartyCompany(createUser.getString("partyId"), delegator);
         }
-        if ("vendor".equals(target)) {
+        if ("supplier".equals(target)) {
             return workEffort.getString("partyId");
         }
         return null;
     }
 
-
-    /**
-     *  传递到合规
-     */
-    public static void returnCompliance(Map<String, Object> oDataContext, Map<String, Object> actionParameters, EdmBindingTarget edmBindingTarget)
-            throws GenericEntityException, OfbizODataException, GeneralServiceException, GenericServiceException {
-        Delegator delegator = (Delegator) oDataContext.get("delegator");
-        LocalDispatcher dispatcher = (LocalDispatcher) oDataContext.get("dispatcher");
-        GenericValue systemUser = Util.getSystemUser(delegator);
-        OdataOfbizEntity boundEntity = Util.getBoundEntity(actionParameters);
-        if (UtilValidate.isEmpty(boundEntity)) {
-            throw new OfbizODataException("Parameter error");
-        }
-        String partyId = (String) boundEntity.getPropertyValue("partyId");
-        String workEffortId = (String) boundEntity.getPropertyValue("workEffortId");
-        String workEffortParentId = (String) boundEntity.getPropertyValue("workEffortParentId");
-        //当前的改为已处理
-        Map<String, Object> updateWorkMap = UtilMisc.toMap(workEffortId, "currentStatusId", "PROCESSED");
-        CommonUtils.setServiceFieldsAndRun(dispatcher.getDispatchContext(), updateWorkMap, "banfftech.updateWorkEffort", systemUser);
-
-        //分配给合规部门
-        String nextWorkEffortId = delegator.getNextSeqId("WorkEffort");
-        Map<String, Object> createWorkMap = UtilMisc.toMap("partyId", partyId, "workEffortId", nextWorkEffortId,
-                "workEffortTypeId", "COWORK_TASK", "currentStatusId", "NOT_PROCESSED", "workEffortParentId", workEffortParentId);
-        CommonUtils.setServiceFieldsAndRun(dispatcher.getDispatchContext(), createWorkMap, "banfftech.createWorkEffort", systemUser);
-        dispatcher.runSync("banfftech.createWorkEffortPartyAssignment",
-                UtilMisc.toMap("userLogin", systemUser, "workEffortId", nextWorkEffortId, "partyId", "HG"));
-    }
 }
