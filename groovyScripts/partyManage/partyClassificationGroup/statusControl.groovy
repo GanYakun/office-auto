a0d74fbe0816c702043470f911c0d327b93765d5
GanYakun <1772894420@qq.com>
2023-11-09 11:50:18 +0800

    add action to create and update classification group

--- groovyScripts/partyManage/partyClassificationGroup/statusControl.groovy
+++ groovyScripts/partyManage/partyClassificationGroup/statusControl.groovy
@@ -11,7 +11,7 @@ import java.util.Map;
 
 module = "partyManage.shipment.statusControl.groovy";
 def generateFields(Map<String, Object> context) {
-    Map<String, Integer> shipmentStatusMap = UtilMisc.toMap("高等风险评级",1,"低等风险评级",3,"中等风险评级",5);
+    Map<String, Integer> shipmentStatusMap = UtilMisc.toMap("HIGH_LEVEL",1,"LOW_LEVEL",3,"MIDDLE_LEVEL",5);
     List<Entity> entityList = context.parameters.get("entityList");
     statusColor = 0L;
     String description = null;
@@ -21,7 +21,12 @@ def generateFields(Map<String, Object> context) {
         if(UtilValidate.isNotEmpty(partyClassificationGroup)){
             description = partyClassificationGroup.getString("description");
         }
-        statusColor = shipmentStatusMap.get(description);
+
+        if(partyClassificationGroup.get("partyClassificationTypeId").equals("RISQUE_RATING")){
+            statusColor = shipmentStatusMap.get(description);
+        }else {
+            statusColor = 0L
+        }
         entity.addProperty(new Property(null, "statusColor", ValueType.PRIMITIVE, statusColor));
     }
     return entityList;
---
+++ src/main/java/com/banfftech/events/SupplierEvents.java
@@ -0,0 +1,71 @@
+package com.banfftech.events;
+
+import com.dpbird.odata.OdataParts;
+import com.dpbird.odata.OfbizODataException;
+import com.dpbird.odata.edm.OdataOfbizEntity;
+import org.apache.ofbiz.base.util.UtilDateTime;
+import org.apache.ofbiz.base.util.UtilMisc;
+import org.apache.ofbiz.base.util.UtilValidate;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.util.EntityUtil;
+import org.apache.ofbiz.service.GenericServiceException;
+import org.apache.ofbiz.service.LocalDispatcher;
+import org.apache.olingo.commons.api.edm.EdmBindingTarget;
+
+import java.util.List;
+import java.util.Map;
+
+public class SupplierEvents {
+    public static Object createPartyClassificationGroup(Map<String, Object> oDataContext, Map<String, Object> actionParameters,
+                                       EdmBindingTarget edmBindingTarget) throws OfbizODataException {
+        Delegator delegator = (Delegator) oDataContext.get("delegator");
+        LocalDispatcher dispatcher = (LocalDispatcher) oDataContext.get("dispatcher");
+        GenericValue userLogin = (GenericValue) oDataContext.get("userLogin");
+        try {
+            List<OdataParts> odataPartsList = (List<OdataParts>) oDataContext.get("odataParts");
+            int odataPartsListSize = odataPartsList.size();
+            GenericValue supplierParty = null;
+            OdataParts odataPartsOne = odataPartsList.get(odataPartsListSize - 2);
+            OdataOfbizEntity supplierPartyEntity = (OdataOfbizEntity) odataPartsOne.getEntityData();
+            supplierParty = supplierPartyEntity.getGenericValue();
+            if (supplierParty == null) {
+                throw new OfbizODataException("Account is invalid");
+            }
+            String partyId = (String) supplierParty.get("partyId");
+
+            String partyClassificationTypeId = (String) actionParameters.get("partyClassificationTypeId");
+            String description = (String) actionParameters.get("description");
+            Map<String, Object> result = dispatcher.runSync("banfftech.createPartyClassificationGroup",
+                    UtilMisc.toMap("partyClassificationTypeId", partyClassificationTypeId,
+                            "description", description, "userLogin", userLogin));
+            String partyClassificationGroupId = (String) result.get("partyClassificationGroupId");
+            dispatcher.runSync("banfftech.createPartyClassification", UtilMisc.toMap("userLogin", userLogin,
+                    "partyId", partyId, "partyClassificationGroupId", partyClassificationGroupId, "fromDate", UtilDateTime.nowTimestamp()));
+            return delegator.findOne("PartyClassificationGroup", true, "partyClassificationGroupId", partyClassificationGroupId);
+        } catch (GenericEntityException | GenericServiceException e) {
+            throw new OfbizODataException(e.getMessage());
+        }
+    }
+
+    public static Object updatePartyClassificationGroup(Map<String, Object> oDataContext, Map<String, Object> actionParameters,
+                                                        EdmBindingTarget edmBindingTarget) throws OfbizODataException {
+        Delegator delegator = (Delegator) oDataContext.get("delegator");
+        LocalDispatcher dispatcher = (LocalDispatcher) oDataContext.get("dispatcher");
+        GenericValue userLogin = (GenericValue) oDataContext.get("userLogin");
+        try {
+            String partyClassificationTypeId = (String) actionParameters.get("partyClassificationTypeId");
+            String description = (String) actionParameters.get("description");
+            OdataOfbizEntity partyClassificationGroupEntity = (OdataOfbizEntity) actionParameters.get("partyClassificationGroup");
+            GenericValue partyClassificationGroup = partyClassificationGroupEntity.getGenericValue();
+            Map<String, Object> result = dispatcher.runSync("banfftech.updatePartyClassificationGroup",
+                    UtilMisc.toMap("partyClassificationTypeId", partyClassificationTypeId, "partyClassificationGroupId", partyClassificationGroup.get("partyClassificationGroupId"),
+                            "description", description, "userLogin", userLogin));
+            String partyClassificationGroupId = (String) result.get("partyClassificationGroupId");
+            return delegator.findOne("PartyClassificationGroup", true, "partyClassificationGroupId", partyClassificationGroupId);
+        } catch (GenericEntityException | GenericServiceException e) {
+            throw new OfbizODataException(e.getMessage());
+        }
+    }
+}
