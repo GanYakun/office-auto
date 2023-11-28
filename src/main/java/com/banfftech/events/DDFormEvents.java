d14ab6b069552e5202740f1ec1ba559c7775bf24
SunCanyang <try028@163.com>
2023-11-28 16:23:53 +0800

    update ddform

---
+++ src/main/java/com/banfftech/events/DDFormEvents.java
@@ -0,0 +1,40 @@
+package com.banfftech.events;
+
+import com.dpbird.odata.OfbizODataException;
+import com.dpbird.odata.edm.OdataOfbizEntity;
+import com.dpbird.odata.handler.annotation.DraftAction;
+import com.dpbird.odata.handler.annotation.DraftEventContext;
+import com.dpbird.odata.handler.annotation.EdmEntity;
+import com.dpbird.odata.handler.annotation.EdmService;
+import org.apache.ofbiz.base.util.UtilMisc;
+import org.apache.ofbiz.base.util.UtilValidate;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.util.EntityQuery;
+import org.apache.ofbiz.service.GenericServiceException;
+import org.apache.ofbiz.service.LocalDispatcher;
+
+@EdmService(edmApp = "supplierForm")
+public class DDFormEvents {
+
+    @EdmEntity(entityTypes = "SupplierParty", action = DraftAction.SAVE_AFTER)
+    public void saveBefore(DraftEventContext context) throws OfbizODataException {
+        LocalDispatcher dispatcher = context.getDispatcher();
+        Delegator delegator = context.getDelegator();
+        GenericValue userLogin = context.getUserLogin();
+        OdataOfbizEntity odataOfbizEntity = context.getOdataOfbizEntity();
+        String partyId = (String) odataOfbizEntity.getPropertyValue("partyId");
+        try {
+            //DDForm保存提交 检索是否勾选敏感问题,添加首次提交标志,更新任务状态
+            dispatcher.runSync("banfftech.ddFormCommitCheck",UtilMisc.toMap("supplierPartyId", partyId, "userLogin", userLogin));
+            GenericValue workEffortTask = EntityQuery.use(delegator).from("WorkEffortAndPartyGroupContact").where("partyId", partyId, "approvePartyId", partyId).queryFirst();
+            if (UtilValidate.isNotEmpty(workEffortTask)) {
+                dispatcher.runSync("banfftech.updateWorkEffort", UtilMisc.toMap("workEffortId", workEffortTask.getString("workEffortId"),
+                        "currentStatusId", "PROCESSED", "userLogin", userLogin));
+            }
+        } catch (GenericServiceException | GenericEntityException e) {
+            throw new OfbizODataException(e.getMessage());
+        }
+    }
+}
