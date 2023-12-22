0a75b7f4b40684ee5cf0ed192778fabce0115439
SunCanyang <try028@163.com>
2023-12-22 13:53:37 +0800

    check DDForm

--- src/main/java/com/banfftech/events/DDFormEvents.java
+++ src/main/java/com/banfftech/events/DDFormEvents.java
@@ -8,6 +8,7 @@ import com.dpbird.odata.handler.annotation.DraftAction;
 import com.dpbird.odata.handler.annotation.DraftEventContext;
 import com.dpbird.odata.handler.annotation.EdmEntity;
 import com.dpbird.odata.handler.annotation.EdmService;
+import org.apache.ofbiz.base.test.BaseUnitTests;
 import org.apache.ofbiz.base.util.Debug;
 import org.apache.ofbiz.base.util.UtilDateTime;
 import org.apache.ofbiz.base.util.UtilMisc;
@@ -15,6 +16,8 @@ import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.condition.EntityCondition;
+import org.apache.ofbiz.entity.condition.EntityOperator;
 import org.apache.ofbiz.entity.util.EntityQuery;
 import org.apache.ofbiz.service.GeneralServiceException;
 import org.apache.ofbiz.service.GenericServiceException;
@@ -41,6 +44,8 @@ public class DDFormEvents {
         if (UtilValidate.isEmpty(boundEntity)) {
             throw new OfbizODataException("Parameter error");
         }
+        //校验必填项
+        checkRequiredField(boundEntity, delegator);
         String partyId = (String) boundEntity.getPropertyValue("partyId");
         GenericValue coWork = EntityQuery.use(delegator).from("WorkEffortAndPartyGroupContact").where("partyId", partyId, "approvePartyId", partyId, "workEffortTypeId", "COWORK_TASK").queryFirst();
         String workEffortId = coWork.getString("workEffortId");
@@ -70,9 +75,58 @@ public class DDFormEvents {
         VendorOnBoardingEmailEvents.ddSubmit(dispatcher, httpServletRequest, boundEntity);
     }
 
-    public static void uboUpload(Map<String, Object> oDataContext, Map<String, Object> actionParameters, EdmBindingTarget edmBindingTarget)
-            throws GenericEntityException, OfbizODataException, GeneralServiceException, GenericServiceException, UnsupportedEncodingException {
-        Debug.log("run ubo Upload =============");
+    public static void checkRequiredField(OdataOfbizEntity vendorEntity, Delegator delegator) throws OfbizODataException, GenericEntityException {
+        String partyId = (String) vendorEntity.getPropertyValue("partyId");
+        checkThrowErr(vendorEntity.getPropertyValue("partyName"));
+        checkThrowErr(vendorEntity.getPropertyValue("dateIncorporation"));
+        checkThrowErr(vendorEntity.getPropertyValue("businessLocation"));
+        checkThrowErr(vendorEntity.getPropertyValue("registrationNumber"));
+        checkThrowErr(vendorEntity.getPropertyValue("workScope"));
+        //check address
+        GenericValue partyContactMechPurpose = EntityQuery.use(delegator).from("PartyContactMechPurpose").where("partyId", partyId, "contactMechPurposeTypeId", "REGISTERED_LOCATION").queryFirst();
+        GenericValue postalAddress = EntityQuery.use(delegator).from("PostalAddress").where("contactMechId", partyContactMechPurpose.getString("contactMechId")).queryFirst();
+        checkThrowErr(postalAddress.getString("countryGeoId"));
+        checkThrowErr(postalAddress.getString("address1"));
+        //check Business Relationship
+        long businessRelationshipCount = EntityQuery.use(delegator).from("PartyRoleType")
+                .where("partyId", partyId, "parentTypeId", "BUSINESS_RELATIONSHIP").queryCount();
+        if (businessRelationshipCount == 0) {
+            throw new OfbizODataException("Please fill in business relationship to company.");
+        }
+
+        //check Contact
+        long contactCount = EntityQuery.use(delegator).from("PartyRelationship").where("partyIdFrom", partyId, "roleTypeIdFrom", "SUPPLIER", "roleTypeIdTo", "CONTACT").queryCount();
+        if (contactCount == 0) {
+            throw new OfbizODataException("Please fill in contact information.");
+        }
+        //check UBO
+        long uboCount = EntityQuery.use(delegator).from("PartyRelationship").where("partyIdFrom", partyId, "roleTypeIdFrom", "SUPPLIER", "roleTypeIdTo", "BENEFICIAL_PERSON").queryCount();
+        if (uboCount == 0) {
+            throw new OfbizODataException("Please fill in ultimate beneficial owner information.");
+        }
+        //check director/officer
+        long directorCount = EntityQuery.use(delegator).from("PartyRelationship").where("partyIdFrom", partyId, "roleTypeIdFrom", "SUPPLIER", "roleTypeIdTo", "SENIOR_STAFF").queryCount();
+        if (directorCount == 0) {
+            throw new OfbizODataException("Please fill in director/officer information.");
+        }
+        //check COMPLIANCE CERTIFICATIONS
+        EntityCondition condition = EntityCondition.makeCondition(
+                EntityCondition.makeCondition("surveyQuestionId", EntityOperator.IN, UtilMisc.toList("9000","9001","9002","9003","9005","9006")),
+                EntityCondition.makeCondition("partyId", partyId));
+        List<GenericValue> surveyQuestionAnswer = EntityQuery.use(delegator).from("SurveyQuestionAnswer").where(condition).queryList();
+        for (GenericValue answer : surveyQuestionAnswer) {
+            String response = answer.getString("enumResponse");
+            if (UtilValidate.isEmpty(response) || ("TRUE".equals(response) && UtilValidate.isEmpty(answer.getString("textResponse")))) {
+                throw new OfbizODataException("Please fill in compliance certifications.");
+            }
+        }
+    }
+
+    private static void checkThrowErr(Object value) throws OfbizODataException {
+        String errMsg = "Please supplement the required fields in the form before submitting.";
+        if (UtilValidate.isEmpty(value)) {
+            throw new OfbizODataException(errMsg);
+        }
     }
 
 }
